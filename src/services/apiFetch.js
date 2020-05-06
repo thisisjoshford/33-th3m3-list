e378e07fb1afdbe3343f0b91ccb195724833fae7
Josh Ford <thisisjoshford@gmail.com>
2020-05-06 16:54:15 -0700

    api fetch

---
+++ src/services/apiFetch.js
@@ -0,0 +1,5 @@
+export const getPeople = () => {
+  return fetch('https://xfiles-api.herokuapp.com/api/v1/characters?perPage=20&page=1')
+    .then(res => res.json())
+    .then(json => console.log(json));
+};
