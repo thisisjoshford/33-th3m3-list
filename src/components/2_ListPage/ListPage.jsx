897bcbd2d17077e438fd8a38d7d62c5af1367cab
Josh Ford <thisisjoshford@gmail.com>
2020-05-06 23:52:46 -0700

    list of names

--- src/components/2_ListPage/ListPage.jsx
+++ src/components/2_ListPage/ListPage.jsx
@@ -5,20 +5,27 @@ const ListPage = () => {
   const { characters } = useContext(XfilesContent);
 
   const elements = characters.map((character) => (
-    <div key={character.id}> 
-      <img src={character.image} />
-      <h3>{character.name}</h3>
-      <h6>{character.occupation}</h6>
-      <p>{character.description}</p>
-    </div>
+    <li key={character.name}>{character.name}</li>
   ));
 
   return (
     <>
       <h3>List of all XFiles Characters</h3>
-      { elements }
+      <ul>
+        { elements }  
+      </ul>
     </>
   );
 };
 
 export default ListPage;
+
+
+// const elements = characters.map((character) => (
+//   <div key={character.id}> 
+//     <img src={character.image} />
+//     <h3>{character.name}</h3>
+//     <h6>{character.occupation}</h6>
+//     <p>{character.description}</p>
+//   </div>
+// ));
