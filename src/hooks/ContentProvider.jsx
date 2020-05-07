dfa3d5d623fe134e6f70b85f2f3e8cb4ce21e179
Josh Ford <thisisjoshford@gmail.com>
2020-05-06 22:09:41 -0700

    fetchin peeps

--- src/hooks/ContentProvider.jsx
+++ src/hooks/ContentProvider.jsx
@@ -1,10 +1,16 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { getPeople } from '../services/apiFetch';
 
 const XfilesContent = createContext();
 
 export const ContentProvider = ({ children }) => {
   const [characters, setCharacters] = useState([]);
 
+  useEffect(() => {
+    getPeople()
+      .then(peeps => setCharacters(peeps));
+
+  });
   return (
     <XfilesContent.Provider value={characters}>
       {children}
