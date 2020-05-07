1ece52250353d8019d55da623d99730d56127cd7
Josh Ford <thisisjoshford@gmail.com>
2020-05-06 23:23:42 -0700

    header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ContentProvider } from '../../hooks/ContentProvider';
-// import Header from '../1_Header/Header';
+import Header from '../1_Header/Header';
 import ListPage from '../2_ListPage/ListPage';
 
 export default function App() {
   return (
     <ContentProvider>
+      <Header />
       <ListPage />
     </ContentProvider>
   );
