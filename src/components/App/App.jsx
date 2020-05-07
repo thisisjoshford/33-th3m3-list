cae29da4801e51d15d739b2ea7dfca8c54e6cdd2
Josh Ford <thisisjoshford@gmail.com>
2020-05-07 16:01:38 -0700

    moved to main for styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { ContentProvider } from '../../hooks/ContentProvider';
-import Header from '../1_Header/Header';
-import ListPage from '../2_ListPage/ListPage';
+import Main from '../Main/Main';
 import { ThemeProvider } from '../../hooks/ThemeProvider';
-import Toggle from '../ThemeToggle/Toggle';
 
 export default function App() {
   return (
     <ContentProvider>
       <ThemeProvider>
-        <Header />
-        <ListPage />
+        <Main />
       </ThemeProvider>
     </ContentProvider>
   );
