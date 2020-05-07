7588738facc57b18a5f2d70b3f3c3170dff8ad49
Josh Ford <thisisjoshford@gmail.com>
2020-05-06 23:37:10 -0700

    api fetch data munge

--- src/services/apiFetch.js
+++ src/services/apiFetch.js
@@ -1,4 +1,5 @@
 export const getPeople = () => {
   return fetch('https://xfiles-api.herokuapp.com/api/v1/characters?perPage=20&page=1')
-    .then(res => res.json());
+    .then(res => res.json())
+    .then(json => json.results);
 };
