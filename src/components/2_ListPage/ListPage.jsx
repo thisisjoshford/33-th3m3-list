ec9b28ca46ef1a16043c1553a61a9226f18be178
Josh Ford <thisisjoshford@gmail.com>
2020-05-06 23:48:33 -0700

    characters rendering!! ;

--- src/components/2_ListPage/ListPage.jsx
+++ src/components/2_ListPage/ListPage.jsx
@@ -2,13 +2,21 @@ import React, { useContext } from 'react';
 import { XfilesContent } from '../../hooks/ContentProvider';
 
 const ListPage = () => {
-  const characters = useContext(XfilesContent);
+  const { characters } = useContext(XfilesContent);
 
-  console.log(characters);
+  const elements = characters.map((character) => (
+    <div key={character.id}> 
+      <img src={character.image} />
+      <h3>{character.name}</h3>
+      <h6>{character.occupation}</h6>
+      <p>{character.description}</p>
+    </div>
+  ));
 
   return (
     <>
       <h3>List of all XFiles Characters</h3>
+      { elements }
     </>
   );
 };
